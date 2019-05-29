277dab410d75389fcea4a058dfbcf4dfab75115f
유근명 <gmyou@onnuridmc.com>
2019-05-29 17:41:24 +0900

    add Transform

--- compress.js
+++ compress.js
@@ -2,8 +2,17 @@ const fs = require('fs');
 const zlib = require('zlib');
 const file = process.argv[2];
 
-fs.createReadStream(file)
-  .pipe(zlib.createGzip())
-  .on('data', () => process.stdout.write('.'))
-  .pipe(fs.createWriteStream(file + '.gz'))
+const { Transform } = require('stream');
+
+const reportProgress = new Transform({  
+  transform(chunk, encoding, callback) {    
+    process.stdout.write('.');    
+    callback(null, chunk);  
+  }
+});
+
+fs.createReadStream(file)  
+  .pipe(zlib.createGzip())  
+  .pipe(reportProgress)  
+  .pipe(fs.createWriteStream(file + '.gz'))  
   .on('finish', () => console.log('Done'));
